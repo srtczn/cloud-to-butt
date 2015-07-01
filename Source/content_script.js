0e41cacf087c75267fa3fc1d454c400f6102cadc
srtczn <srtczn@gmail.com>
2015-07-01 11:21:30 +0300

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bThe God\b/g, "Sertac");
+	v = v.replace(/\bThe god\b/g, "Sertac");
+	v = v.replace(/\bthe God\b/g, "Sertac");
+	v = v.replace(/\bthe god\b/g, "Sertac");
 	
 	textNode.nodeValue = v;
 }
