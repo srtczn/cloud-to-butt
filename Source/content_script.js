fcb95cb2e5cd4f2291c6e57e63f706247cbd3d9e
srtczn <srtczn@gmail.com>
2015-07-01 11:55:02 +0300

    OMG added

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,19 +1,19 @@
 walk(document.body);
 
-function walk(node) 
+function walk(node)
 {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
-	
+
 	var child, next;
 
-	switch ( node.nodeType )  
+	switch ( node.nodeType )
 	{
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
 			child = node.firstChild;
-			while ( child ) 
+			while ( child )
 			{
 				next = child.nextSibling;
 				walk(child);
@@ -27,7 +27,7 @@ function walk(node)
 	}
 }
 
-function handleText(textNode) 
+function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
@@ -35,8 +35,7 @@ function handleText(textNode)
 	v = v.replace(/\bMy god\b/g, "Sertac");
 	v = v.replace(/\bmy God\b/g, "Sertac");
 	v = v.replace(/\bmy god\b/g, "Sertac");
-	
+	v = v.replace(/\bOMG\b/g, "Oh Sertac");
+
 	textNode.nodeValue = v;
 }
-
-
